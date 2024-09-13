f4aeab888358fe14a334fa4f777e5ee79f97c9c1
Charlie Lye <karl.lye@gmail.com>
2024-09-13 13:00:18 +0000

    Cleanup

--- src/pedersen/pedersen.zig
+++ src/pedersen/pedersen.zig
@@ -3,12 +3,6 @@ const msm = @import("../msm/naive.zig").msm;
 
 pub fn commit(comptime G1: type, inputs: []const G1.Fr, generator_offset: u32) G1.Element {
     const gen = generators.default_generators[generator_offset..inputs.len];
-
-    // var acc = G1.Element.infinity;
-    // for (0..inputs.len) |i| {
-    //     acc = acc.add(gen[i].mul(G1.Fr.from_int(inputs[i].to_int())));
-    // }
-
     return msm(G1, inputs, gen[0..inputs.len]).normalize();
 }
 
